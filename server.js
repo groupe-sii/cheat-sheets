331f95fcb28a4389fa55c9f44dbb5f429703e6bc
Kevin Grandemange <kgrandemange@sii-ouest.fr>
2016-12-19 11:12:13 +0100

    refactoring
    each cheat sheet in one folder for a better web integration

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.get("/", (req, res) => {
     res.redirect("/firstSide");
 });
 
-app.get("/firstSide", (req, res) => {
-    res.sendFile(__dirname + '/dist/firstSide/firstSide.html')
-});
-
-app.get("/reverse", (req, res) => {
-    res.sendFile(__dirname + '/dist/reverse/reverse.html')
-});
+// app.get("/firstSide", (req, res) => {
+//     res.sendFile(__dirname + '/dist/git/first-side/first-side.html')
+// });
+//
+// app.get("/reverse", (req, res) => {
+//     res.sendFile(__dirname + '/dist/git/reverse/reverse.html')
+// });
 
 app.listen(port, () => {
         console.log("Exppres Node.js server running on port " + port);
