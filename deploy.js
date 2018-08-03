baa0f0675aa3c0f2b1408d81eb581306d285aa31
Loïc C <loic.carney@gmail.com>
2018-08-03 15:19:34 +0200

    Mise à jour du déploiement pour repo perso

--- deploy.js
+++ deploy.js
@@ -6,12 +6,12 @@ var exec = require('child_process').exec,
 // Shell gitCommands
 var gitCommands = {
     currentBranch:  "git branch | grep \\* | cut -d ' ' -f2",
-    clone:          "git clone -b gh-pages https://github.com/loicc04/cheatSheets.git gh-pages",
+    clone:          "git clone -b gh-pages https://github.com/LoicC04/cheat-sheets.git gh-pages",
     configUser:     "git config user.name \"Travis-CI\"",
     configEmail:    "git config user.email \"lcarney@sii.fr\"",
     add:            "git add --all",
     commit:         "git commit -am \"Automatic build from Travis-CI\"",
-    push:           "git push \"https://${GH_TOKEN}@github.com/loicc04/cheatSheets.git\" gh-pages:gh-pages"
+    push:           "git push \"https://${GH_TOKEN}@github.com/LoicC04/cheat-sheets.git\" gh-pages:gh-pages"
 };
 
 var DEPLOY_BRANCH = 'master';
